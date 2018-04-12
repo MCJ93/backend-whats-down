f60fd55f177e16c6d0a8d783738812f0614d50b6
unknown <macjejj@gmail.com>
2018-04-12 19:13:39 +0200

    init

---
+++ main.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var url = require("url"); 
+var querystring = require('querystring'); //Divides query params
+var EventEmitter = require("events").EventEmitter; //Allow emitting events
+
+
+var server = http.createServer();  
+server.on("request", function(req, res) { //Created on request
+  var page = url.parse(req.url).pathname; //parse req to get requested url page
+  console.log(page); //log in nodeJS console (not in browser)
+  var query = url.parse(req.url).query; //get query params
+  var params = querystring.parse(url.parse(req.url).query); //get query params eg. params["firstName"]
+  console.log(query);
+  res.writeHead(200, {"Content-Type": "text/html"}); //Header - have to be placed BEFORE res.write();
+  if (page == "/") {
+    res.write("HOme");  
+  } else {
+    res.write("Not home");
+  }
+  res.end();  //Always have to end a response.
+  });
+
+server.on("close", function() { //event listener
+  console.log("On close");
+})
+
+server.listen(8080);
+
+var EventEmitter = require('events').EventEmitter; //create event
+var game = new EventEmitter();
+
+game.on('gameover', function(message){  //event listener
+    console.log(message);
+});
+game.emit('gameover', 'You lose!');
+
+game.emit('newplayer', 'Mario', 35); // Sends the name of a new player who’s just arrived and gives their age
